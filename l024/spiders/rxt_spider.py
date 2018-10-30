3d020e7107b8e99fdbf074f90042455ac5dbf91b
xiyushiyi_work@163.com <xiyushiyi_work@163.com>
2018-10-31 00:10:27 +0800

    fix

--- l024/spiders/rxt_spider.py
+++ l024/spiders/rxt_spider.py
@@ -1,14 +1,31 @@
-import scrapy
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
 
-class RxtSpider(scrapy.Spider):
+import urlparse
+import scrapy
+from l024.items import L0L0Item
+import pdb
+
+class DmozSpider(scrapy.Spider):
     name = "rxt"
-    allowed_domains = ["dmoz.org"]
+    # allowed_domains = ["xinli001.com"]
     start_urls = [
-        "http://www.dmoz.org/Computers/Programming/Languages/Python/Books/",
-        "http://www.dmoz.org/Computers/Programming/Languages/Python/Resources/"
+        "http://1024.clsmik.pw/pw/thread-htm-fid-3.html"
     ]
 
     def parse(self, response):
-        filename = response.url.split("/")[-2]
-        with open(filename, 'wb') as f:
-            f.write(response.body)
+        rootpath = response.xpath('//*[@id="ajaxtable"]//tbody/tr/td[2]/h3/a')
+        print "----------------------------------------"
+        for it in rootpath:
+            item = L0L0Item()
+            # pdb.set_trace()
+            item['link'] = urlparse.urljoin(response.url,it.xpath('@href').extract_first())
+            item['title'] = it.xpath('text()').extract_first()
+            if item['title']:
+
+                yield item
+            else:
+                pass
